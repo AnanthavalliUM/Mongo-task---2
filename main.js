baffa86da9c065e11fe5d4143d4dab1d8c22d50e
Ananthavalli <100471735+AnanthavalliUM@users.noreply.github.com>
2022-08-18 16:33:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,261 @@
+db.zen.insertMany([ {users:  "Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi "   ,
+codekata:  "65 , 12 , 45 , 55 , 50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75 "   ,
+attendance:{present: 15   ,absent:0   ,date:6},
+topics: "Bascis of Javascript" ,
+tasks:{name: "Write a code to rotate an array by 3 times" ,submit:12 ,notsubmit:3 },
+company_drives:{name: "Amazon" ,date:7,user_appeared: "Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi "   },
+mentors:{name: "Sanjay Sarvanan" ,mentee:20}
+},
+{users:  "Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi "   ,
+codekata:  "65 , 12 , 45 , 55 , 50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70, 85, 75"   ,
+attendance:{present: 11,absent:4,date:7},
+topics: "MRF concepts in JS" ,
+tasks:{name: "Write a code to reverse a string input" ,submit:10 ,notsubmit:5  },
+company_drives:{name: "Facebook" ,date:8,user_appeared:  "Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi "}  ,
+mentors:{name: "Ananthkumar" ,mentee:13}
+},
+
+{users:  "Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi "   ,
+codekata:  "65 , 12 , 45 , 55 , 50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75"   ,
+attendance:{present:11 ,absent:4   ,date:9},
+topics: "Oops concepts in JS" ,
+tasks:{name: "Write a code by using constructor for the movies" ,submit:14, notsubmit: 1 },
+company_drives:{name: "Instagram" ,date:10,user_appeared: "Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi "  },
+mentors:{name: "Ananthkumar" ,mentee:20}
+},
+{users:  "Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi "  ,
+codekata:  "50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 "  ,
+attendance:{present:13 ,absent: 2  ,date:15},
+topics: "CSS  concepts" ,
+tasks:{name: "Design a page with flex box concepts" ,submit: 11,notsubmit: 4 },
+company_drives:{name: "TCS" ,date:15,user_appeared: "Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi " },
+mentors:{name: "Sanjay Sarvanan" ,mentee:16}
+},
+{users:   "Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi " ,
+codekata:  "12 , 45 , 55 , 50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75 , 57"   ,
+attendance:{present:10,absent:5  ,date:16},
+topics: "HTML concepts" ,
+tasks:{name: "Design a webpage by using basic html and css concepts" ,submit:14 ,notsubmit: 1},
+company_drives:{name: "Microsoft" ,date:16,user_appeared:  "Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi "   },
+mentors:{name: "Sai Mohan" ,mentee:19}
+},
+{users: "Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi "  ,
+codekata:  "15 , 21 , 51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 , 40, 25, 40, 35"   ,
+attendance:{present:10,absent:5  ,date:17},
+topics: "DOM concepts" ,
+tasks:{name: "Write a code to perform calculator operations using DOM  " ,submit:8 ,notsubmit: 7 },
+company_drives:{name: "CTS" ,date:19,user_appeared:  "Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi "  },
+mentors:{name: "Sai Mohan" ,mentee:12}
+},
+{users:  "Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi "   ,
+codekata:  "51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 , 40, 76, 45, 30, 50, 20"   ,
+attendance:{present:10,absent:5   ,date:18},
+topics: "React JS" ,
+tasks:{name: "Write a code in react by using components" ,submit:9,notsubmit:6   },
+company_drives:{name: "Infosys" ,date:19,user_appeared:  "Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi "   },
+mentors:{name: "Raghav kumar" ,mentee:10}
+},
+{users:  "Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi "  ,
+codekata:  "65 , 55 , 50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75 , 57 , 66 "   ,
+attendance:{present:11 ,absent: 4  ,date:29},
+topics: "NodeJS-File system" ,
+tasks:{name: "Write a code to excecute the file system task" ,submit:15,notsubmit:0   },
+company_drives:{name: "Accenture" ,date:23,user_appeared: "Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi " },
+mentors:{name: "Raghav kumar" ,mentee:18}
+},
+{users:  "Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi " ,
+codekata:  "55 , 50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 , 40"   ,
+attendance:{present:15,absent: 2  ,date:30},
+topics: "NodeJS-CRUD operations" ,
+tasks:{name: "Write a code to execute the CRUD operations in NodeJS" ,submit: 10,notsubmit: 5 },
+company_drives:{name: "Twitter" ,date:24,user_appeared:  "Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi "  },
+mentors:{name: "Shyam Prasad" ,mentee:17}
+}
+]);
+
+
+1. Find all the topics and tasks which are thought in the month of October
+db.zen.find({},{_id:0,topics:1,tasks:{name:1}})
+[
+{
+    topics: 'Bascis of Javascript',
+    tasks: { name: 'Write a code to rotate an array by 3 times' }
+  },
+  {
+    topics: 'MRF concepts in JS',
+    tasks: { name: 'Write a code to reverse a string input' }
+  },
+  {
+    topics: 'Oops concepts in JS',
+    tasks: { name: 'Write a code by using constructor for the movies' }
+  },
+  {
+    topics: 'CSS  concepts',
+    tasks: { name: 'Design a page with flex box concepts' }
+  },
+  {
+    topics: 'HTML concepts',
+    tasks: { name: 'Design a webpage by using basic html and css concepts' }
+  },
+  {
+    topics: 'DOM concepts',
+    tasks: {
+      name: 'Write a code to perform calculator operations using DOM  '
+    }
+  },
+  {
+    topics: 'React JS',
+    tasks: { name: 'Write a code in react by using components' }
+  },
+  {
+    topics: 'NodeJS-File system',
+    tasks: { name: 'Write a code to excecute the file system task' }
+  },
+  {
+    topics: 'NodeJS-CRUD operations',
+    tasks: { name: 'Write a code to execute the CRUD operations in NodeJS' }
+  }
+]
+Find all the company drives which appeared between 15 oct-2020 and 31-oct-2020
+db.zen.find({
+   "company_drives.date": {
+    $gte: 15, $lte:31
+   }
+}, 
+   {
+    _id:0, company_drives:{
+        name:1, date:1
+    }
+   }
+)
+[
+    { company_drives: { name: 'TCS', date: 15 } },
+    { company_drives: { name: 'Microsoft', date: 16 } },
+    { company_drives: { name: 'CTS', date: 19 } },
+    { company_drives: { name: 'Infosys', date: 19 } },
+    { company_drives: { name: 'Accenture', date: 23 } },
+    { company_drives: { name: 'Twitter', date: 24 } }
+  ]
+
+  3.Find all the company drives and students who are appeared for the placement.
+  db.zen.find({},{_id:0,company_drives:{name: 1, user_appeared:1}})
+  [
+    {
+      company_drives: {
+        name: 'Amazon',
+        user_appeared: 'Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi '       
+      }
+    },
+    {
+      company_drives: {
+        name: 'Facebook',
+        user_appeared: 'Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi '       
+      }
+    },
+    {
+      company_drives: {
+        name: 'Instagram',
+        user_appeared: 'Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi '       
+      }
+    },
+    {
+      company_drives: {
+        name: 'TCS',
+        user_appeared: 'Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi '       
+      }
+    },
+    {
+      company_drives: {
+        name: 'Microsoft',
+        user_appeared: 'Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi '       
+      }
+    },
+    {
+      company_drives: {
+        name: 'CTS',
+        user_appeared: 'Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi '       
+      }
+    },
+    {
+      company_drives: {
+        name: 'Infosys',
+        user_appeared: 'Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi '       
+      }
+    },
+    {
+      company_drives: {
+        name: 'Accenture',
+        user_appeared: 'Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi '       
+      }
+    },
+    {
+      company_drives: {
+        name: 'Twitter',
+        user_appeared: 'Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi '       
+      }
+    }
+  ] 
+
+  4.Find the number of problems solved by the user in codekata
+  db.zen.find({},{_id:0,users:1, codekata:1})
+  [
+    {
+      users: 'Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi ',
+      codekata: '65 , 12 , 45 , 55 , 50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75 '
+    },
+    {
+      users: 'Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi ',
+      codekata: '65 , 12 , 45 , 55 , 50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70, 85, 75'
+    },
+    {
+      users: 'Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi ',
+      codekata: '65 , 12 , 45 , 55 , 50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75'
+    },
+    {
+      users: 'Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi ',
+      codekata: '50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 '
+    },
+    {
+      users: 'Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi ',
+      codekata: '12 , 45 , 55 , 50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75 , 57'
+    },
+    {
+      users: 'Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi ',
+      codekata: '15 , 21 , 51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 , 40, 25, 40, 35'
+    },
+    {
+      users: 'Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi ',
+      codekata: '51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 , 40, 76, 45, 30, 50, 20'
+    },
+    {
+      users: 'Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi ',
+      codekata: '65 , 55 , 50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75 , 57 , 66 '
+    },
+    {
+      users: 'Ravi , Meena , Karthik , Arjun , Geetha , Samuktha , Raja , Anand , Shyam , Karthiga , Tamana , Tanuja, Jyothi, Deepa , Shakthi ',
+      codekata: '55 , 50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 , 40'
+    }
+  ]
+
+  5.Find all the mentors with who has the mentees count more than 15
+  db.zen.find({"mentors.mentee":{$gt:15}},{_id:0,mentors:1}).sort({"mentors.mentee":1})
+
+  [
+    { mentors: { name: 'Sanjay Sarvanan', mentee: 16 } },
+    { mentors: { name: 'Shyam Prasad', mentee: 17 } },
+    { mentors: { name: 'Raghav kumar', mentee: 18 } },
+    { mentors: { name: 'Sai Mohan', mentee: 19 } },
+    { mentors: { name: 'Sanjay Sarvanan', mentee: 20 } },
+    { mentors: { name: 'Ananthkumar', mentee: 20 } }
+  ]
+
+  6. Find the number of users who are absent and task is not submitted  between 15 oct-2020 and 31-oct-2020
+  db.zen.find({"attendance.date":{$gte: 15 , $lte: 31}},{_id:0,attendance:{absent:1, date:1}, task:{notsubmit:1}})
+
+ 
+
+
+
+
+
+
